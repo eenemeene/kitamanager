06b7970359dbe697bb0f58fd4904f2f94f9e17d0
Thomas Bechtold <thomasbechtold@jpberlin.de>
2024-01-12 15:34:16 +0100

    add .github workflow

--- kmsite/urls.py
+++ kmsite/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('', include('kitamanager.urls')),
 ]
 
