f47f6e24954ff0ab5e4c98c89ecc191d0b623691
Thomas Bechtold <thomasbechtold@jpberlin.de>
2024-01-14 07:42:37 +0100

    Add docker-compose for easy start
    To get the application up & running, docker-compose can be used
    now. That's useful to setup the database together with the kitamanager
    app in a relative simple way.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -eux
 
+# FIXME: ugly wait until DB is ready and created
+sleep 5
+
 python3 manage.py collectstatic --noinput --clear
+python3 manage.py compilemessages
 python3 manage.py makemigrations
 python3 manage.py migrate
 
