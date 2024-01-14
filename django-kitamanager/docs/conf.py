5009a61dd52fe35965b688941d10e437248a8965
Thomas Bechtold <thomasbechtold@jpberlin.de>
2024-01-14 20:10:35 +0100

    docs: add basic documentation structure

---
+++ django-kitamanager/docs/conf.py
@@ -0,0 +1,29 @@
+from typing import List
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "kitamanager"
+copyright = "2024, Thomas Bechtold"
+author = "Thomas Bechtold"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions: List[str] = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+language = "de"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
